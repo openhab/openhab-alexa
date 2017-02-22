6b1a34aa166bc7cf1f3c719ae23c1eec9552387e
paphko <git@paphko.de>
2017-02-22 05:05:04 +0100

    Remove OH1 because it doesn't work with current impl (Homekit tags). (#8)
    Link added to a fork that supports OH1 via a dedicated Alexa group.

--- rest.js
+++ rest.js
@@ -15,15 +15,15 @@ var utils = require('./utils.js');
 /**
  * Returns all items
  */
-function getItems(version, token, success, failure) {
-    return getItem(version, token, null, success, failure);
+function getItems(token, success, failure) {
+    return getItem(token, null, success, failure);
 }
 
 /**
  * Returns a single item
  */
-function getItem(version, token, itemName, success, failure) {
-    var options = httpItemOptions(version, token, itemName);
+function getItem(token, itemName, success, failure) {
+    var options = httpItemOptions(token, itemName);
     // DEBUG
     utils.log("GET", "https://" + options.hostname + ":" + options.port + options.path);
 
@@ -53,8 +53,8 @@ function getItem(version, token, itemName, success, failure) {
 /**
  * POST a command to a item
  **/
-function postItemCommand(version, token, itemName, value, success, failure) {
-    var options = httpItemOptions(version, token, itemName, 'POST', value.length);
+function postItemCommand(token, itemName, value, success, failure) {
+    var options = httpItemOptions(token, itemName, 'POST', value.length);
 
     // DEBUG
     utils.log("POST", "https://" + options.hostname + ":" + options.port + options.path + " value " + value);
@@ -81,11 +81,11 @@ function postItemCommand(version, token, itemName, value, success, failure) {
 /**
  * Returns a http option object sutiable for item commands
  */
-function httpItemOptions(version, token, itemname, method, length) {
+function httpItemOptions(token, itemname, method, length) {
     var options = {
         hostname: config.host,
         port: config.port,
-        path: config.path + (itemname || "") + ( version == 1 ? '?type=json' : ''),
+        path: config.path + (itemname || ""),
         method: method || 'GET',
         headers: {}
     };
@@ -96,11 +96,6 @@ function httpItemOptions(version, token, itemname, method, length) {
         options.headers['Authorization'] = "Bearer " + token;
     }
 
-    // If no itemname was spcified; OH version is 1, we are in discovery mode - use OH1 discovery_path defined in config.js
-    if (itemname === null && version === 1) {
-        options.path = config.discovery_path + '?type=json';
-    }
-
     if (method === 'POST' || method === 'PUT') {
         options.headers['Content-Type'] = 'text/plain';
         options.headers['Content-Length'] = length;
@@ -108,48 +103,6 @@ function httpItemOptions(version, token, itemname, method, length) {
     return options;
 }
 
-/**
- * Determines the openHAB version of a server
- */
-function getOpenhabVersion(token, cb) {
-    var options = {
-        hostname: config.host,
-        port: config.port,
-        path: '/rest/bindings',
-        method: 'GET',
-        headers: {}
-    };
-
-    if (config.userpass) {
-        options.auth = config.userpass;
-    } else {
-        options.headers['Authorization'] = "Bearer " + token;
-    }
-
-    // DEBUG
-    utils.log("getOpenhabVersion", options.hostname + ":" + options.port + options.path);
-
-    https.get(options, function (response) {
-        utils.log("getOpenhabVersion", "statusCode " + response.statusCode);
-        switch (response.statusCode) {
-        case 200:
-            cb(2);
-            break;
-        case 404:
-            cb(1);
-            break;
-        default:
-            cb(0);
-            break;
-        }
-        response.on("error", function (e) {
-            utils.log("OPENHAB ERROR: " + e.message);
-            cb(0);
-        });
-    }).end();
-}
-
 module.exports.getItems = getItems;
 module.exports.getItem = getItem;
 module.exports.postItemCommand = postItemCommand;
-module.exports.getOpenhabVersion = getOpenhabVersion;
