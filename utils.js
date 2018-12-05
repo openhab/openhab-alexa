57481b601501d2455194500443ace0e3dc4ada92
clifford foster <cliffordnfoster@gmail.com>
2018-12-05 07:00:59 +0000

    Added v3 tagging back into the file

--- utils.js
+++ utils.js
@@ -11,6 +11,7 @@
  * Define alexa capability namespace format pattern
  **/
  var CAPABILITY_PATTERN = /^(?:Alexa\.)?(\w+)\.(\w+)$/;
+ var TAG_PATTERN = /^Alexa\.(\w+)\.(\w+)(?::(\S+))?/;
 
  /**
  * Define alexa supported display categories
@@ -194,8 +195,99 @@ function metadataToPropertyMap(item, propertyMap = {}) {
   return propertyMap;
 }
 
+/**
+ * Creates/Modifies a map structure to assoicate items to an endpoint from tags, will return a new map
+ * if propertyMap is omitted or null, otherwise will modify the existing map (and return it as well)
+ * eg:
+ *
+ * OH Tags
+ *
+ * Number FooTargetSetPoint "Foo Target SetPoint" ["Alexa.ThermostatController.targetSetpoint:scale=Fahrenheit"]
+ * Number FooUpperSetPoint  "Foo Upper SetPoint"  ["Alexa.ThermostatController.upperSetpoint:scale=Fahrenheit"]
+ * Number FooLowerSetPoint  "Foo Lower SetPoint"  ["Alexa.ThermostatController.lowerSetpoint:scale=Fahrenheit"]
+ * String FooMode           "Foo Mode"            ["Alexa.ThermostatController.thermostatMode:OFF=0,HEAT=1,COOL=2,AUTO=3"
+ * Switch FooSwitch         "FooSwitch"           ["Alexa.PowerController.powerState"]
+ *
+ * returns
+ *
+  * propertyMap:
+  *  {
+  *    ThermostatController : {
+  *      targetSetpoint : {
+  *          itemName: "FooTargetSetPoint",
+  *          parameters: {
+  *            scale : "Fahrenheit",
+  *         }
+  *      },
+  *      upperSetpoint : {
+  *          itemName: "FooTargetSetPoint",
+  *          parameters: {
+  *            scale : "Fahrenheit",
+  *         }
+  *      },
+  *      lowerSetpoint : {
+  *          itemName: "FooTargetSetPoint",
+  *          parameters: {
+  *            scale : "Fahrenheit",
+  *         }
+  *      },
+  *      thermostatMode : {
+  *          itemName: "FooMode",
+  *          parameters: {
+  *            OFF : "0",
+  *            HEAT : "1",
+  *            COOL : "2",
+  *            AUTO : "3"
+  *         }
+  *      }
+  *    },
+  *    PowerController : {
+  *      powerState : {
+  *         itemName: "FooSwitch"
+  *       }
+  *    }
+ * @param {String} item
+ * @param {object} propertyMap
+ */
+function tagsToPropertyMap(item, propertyMap = {}) {
+  item.tags.forEach(function (tag) {
+    var matches;
+    if ((matches = TAG_PATTERN.exec(tag)) !== null) {
+      var interfaceName = matches[1];
+      var property = matches[2];
+      var parameters = matches[3];
+
+      if (!propertyMap[interfaceName]) {
+        propertyMap[interfaceName] = {};
+      }
+      propertyMap[interfaceName][property] = {};
+      propertyMap[interfaceName][property].parameters = {};
+      propertyMap[interfaceName][property].itemName = item.name;
+      if (parameters) {
+        var params = parameters.split(",");
+        params.forEach(function (param) {
+          var keyValue = param.split("=");
+          if (keyValue.length == 2) {
+            //if a tag has a category parameter add this to the interface instead of the property
+            if(keyValue[0] == 'category'){
+              if(!propertyMap[interfaceName].categories){
+                propertyMap[interfaceName].categories = [];
+              }
+              propertyMap[interfaceName].categories.push(keyValue[1].toUpperCase());
+            } else {
+              propertyMap[interfaceName][property].parameters[keyValue[0]] = keyValue[1];
+            }
+          }
+        });
+      }
+    }
+  });
+  return propertyMap;
+}
+
 module.exports.date = date;
 module.exports.metadataToPropertyMap = metadataToPropertyMap;
+module.exports.tagsToPropertyMap = tagsToPropertyMap;
 module.exports.normalizeColorTemperature = normalizeColorTemperature;
 module.exports.normalizeThermostatMode = normalizeThermostatMode;
 module.exports.supportedDisplayCategory = supportedDisplayCategory;
