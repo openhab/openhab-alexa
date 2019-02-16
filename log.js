2cabfe86cf02d8ab06053462e6a1d7e2a51653ca
digitaldan <dan@digitaldan.com>
2019-02-15 17:01:59 -0800

    Switch to using winston as a logger
    Signed-off-by: digitaldan <dan@digitaldan.com>

--- log.js
+++ log.js
@@ -1,52 +1,15 @@
-/**
- * Logging helpers for lambda
- * Set enviroment variable LOG_LEVEL to one of the following LEVELS to control
- * verbosity of logging.
- */
+let winston = require('winston');
 
-var LEVELS = [
-    'TRACE',
-    'DEBUG',
-    'INFO',
-    'WARN',
-    'ERROR',
-];
+// define the custom settings for each transport (file, console)
+let setLevel = process.env.LOG_LEVEL !== null ? process.env.LOG_LEVEL.toLowerCase() : "debug";
 
-var DEFAULT = 'DEBUG';
+const logger = winston.createLogger({
+    level: setLevel,
+    levels : { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, trace: 5 },
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+  });
 
-function log(level, message) {
-    var setLevel = LEVELS.indexOf(process.env.LOG_LEVEL ?
-        process.env.LOG_LEVEL : DEFAULT);
-    if (LEVELS.indexOf(level) < setLevel) {
-        return;
-    }
-    switch (level) {
-    case 'INFO':
-        console.info(message);
-        break;
-    case 'WARN':
-        console.warn(message);
-        break;
-    case 'ERROR':
-        console.error(message);
-        break;
-    default: // debug, trace
-        console.log(message);
-        break;
-    }
-}
-module.exports.trace = function (message) {
-    log('TRACE', message);
-};
-module.exports.debug = function (message) {
-    log('DEBUG', message);
-};
-module.exports.info = function (message) {
-    log('INFO', message);
-};
-module.exports.warn = function (message) {
-    log('WARN', message);
-};
-module.exports.error = function (message) {
-    log('ERROR', message);
-};
+module.exports = logger;
