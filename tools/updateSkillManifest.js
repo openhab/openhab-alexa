d75e089ec4b66341c1dba9976e3efde8ca9227bf
Jeremy <jsetton@users.noreply.github.com>
2019-11-11 13:57:14 -0500

    Add hindi language to skill supported list (#305)
    Signed-off-by: jsetton <jeremy.setton@gmail.com>

--- tools/updateSkillManifest.js
+++ tools/updateSkillManifest.js
@@ -32,7 +32,7 @@ const RESOURCES_PATH = '../resources';
  */
 const LOCALES = [
   'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'es-ES', 'es-MX', 'es-US',
-  'fr-CA', 'fr-FR', 'it-IT', 'ja-JP', 'pt-BR'
+  'fr-CA', 'fr-FR', 'hi-IN', 'it-IT', 'ja-JP', 'pt-BR'
 ];
 
 /**
