3d8b7a8f6a8cfe45c9327a75619b79cc19bb6b51
Jeremy <jsetton@users.noreply.github.com>
2020-01-21 21:59:28 -0500

    Limit friendly names restriction to capability resources only (#350)
    Signed-off-by: jsetton <jeremy.setton@gmail.com>

--- lambda/smarthome/alexa/v3/capabilities.js
+++ lambda/smarthome/alexa/v3/capabilities.js
@@ -124,7 +124,7 @@ function getCapabilityInterface(interfaceName, properties, settings = {}) {
     // Get capability resources if friendly names parameter defined
     if (parameters.friendlyNames) {
       Object.assign(resources, getResourcesObject({
-        labels: parameters.friendlyNames, language: parameters.language || language}));
+        labels: parameters.friendlyNames, language: parameters.language || language, type: 'capability'}));
     }
 
     // Get capability semantics if action or state mappings parameter defined
@@ -170,7 +170,7 @@ function getCapabilityInterface(interfaceName, properties, settings = {}) {
           'supportedModes': parameters.supportedModes.map(mode => ({
             'value': mode.split('=').shift(),
             'modeResources': getResourcesObject({
-              labels: mode.split(/[=:]/).slice(1), language: parameters.language || language})
+              labels: mode.split(/[=:]/).slice(1), language: parameters.language || language, type: 'mode'})
           }))
         });
         break;
@@ -186,7 +186,7 @@ function getCapabilityInterface(interfaceName, properties, settings = {}) {
           'presets': parameters.presets.map(preset => ({
             'rangeValue': parseInt(preset.split('=').shift()),
             'presetResources': getResourcesObject({
-              labels: preset.split(/[=:]/).slice(1), language: parameters.language || language})
+              labels: preset.split(/[=:]/).slice(1), language: parameters.language || language, type: 'preset'})
           }))
         });
         break;
@@ -271,8 +271,9 @@ function getResourcesObject(parameters = {}) {
           });
         }
       } else if (label) {
-        // Add text object for each locale matching language parameter, if friendly name supported
-        if (isSupportedFriendlyName(label)) {
+        // Add text object for each locale matching language parameter,
+        //  if not capability resources type or if friendly name supported
+        if (parameters.type !== 'capability' || isSupportedFriendlyName(label)) {
           LOCALES.filter(locale => locale.split('-')[0] === parameters.language.toLowerCase()).forEach((locale) => {
             names.push({
               '@type': 'text',
