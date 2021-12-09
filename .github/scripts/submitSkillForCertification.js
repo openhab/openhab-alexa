6a5922db8d801e80cc20f0880794e337bfe9063a
jsetton <jeremy.setton@gmail.com>
2021-12-09 02:33:34 -0500

    Add wait logic for retrieving skill validations
    Signed-off-by: jsetton <jeremy.setton@gmail.com>

--- .github/scripts/submitSkillForCertification.js
+++ .github/scripts/submitSkillForCertification.js
@@ -12,8 +12,28 @@
  */
 
 const assert = require('assert');
-const { getCommandOuput, loadSchema } = require('./utils');
+const { getCommandOuput, loadSchema, sleep } = require('./utils');
 const { SKILL_MANIFEST_FILE } = require('./constants');
+
+/**
+ * Returns skill validations for a given id
+ * @param  {String} id
+ * @return {Object}
+ */
+async function getSkillValidations(id) {
+  const command = `ask smapi get-skill-validations -s ${process.env.SKILL_ID} -i ${id} -g development`;
+  let validations;
+
+  // Retrieve skill validations up to 10 times while status is in progress
+  for (let tries = 0; (!validations || validations.status === 'IN_PROGRESS') && tries < 10; tries++) {
+    // Wait for 1 second
+    await sleep(1000);
+    // Get validations command output
+    validations = getCommandOuput(command, { json: true });
+  }
+
+  return validations;
+}
 
 /**
  * Submits skill for certification
@@ -36,18 +56,17 @@ function submitSkillForCertification() {
  *  https://developer.amazon.com/docs/smapi/ask-cli-command-reference.html#submit-skill-validation-subcommand
  *  https://developer.amazon.com/docs/smapi/ask-cli-command-reference.html#get-skill-validations-subcommand
  */
-function validateSkill() {
+async function validateSkill() {
   // Retrieve skill locales from manifest
   const { manifest } = loadSchema(SKILL_MANIFEST_FILE);
   const locales = Object.keys(manifest.publishingInformation.locales).join(',');
 
   // Request skill validation
-  const submitCommand = `ask smapi submit-skill-validation -s ${process.env.SKILL_ID} -l ${locales} -g development`;
-  const { id } = getCommandOuput(submitCommand, { json: true });
+  const command = `ask smapi submit-skill-validation -s ${process.env.SKILL_ID} -l ${locales} -g development`;
+  const { id } = getCommandOuput(command, { json: true });
 
-  // Get skill validations result
-  const getCommand = `ask smapi get-skill-validations -s ${process.env.SKILL_ID} -i ${id} -g development`;
-  const { result, status } = getCommandOuput(getCommand, { json: true });
+  // Get skill validations
+  const { result, status } = await getSkillValidations(id);
 
   // Log result if provided
   if (result) console.log('Result:', JSON.stringify(result, null, 2));
@@ -55,14 +74,21 @@ function validateSkill() {
   assert.equal(status, 'SUCCESSFUL', `Skill validation ${status.replace(/_/g, ' ').toLowerCase()}`);
 }
 
-if (require.main === module) {
+/**
+ * Main async function
+ */
+async function main() {
   try {
     // Validate skill
-    validateSkill();
+    await validateSkill();
     // Submit skill for certification
     submitSkillForCertification();
   } catch (error) {
     console.log(error.message);
     process.exit(1);
   }
+}
+
+if (require.main === module) {
+  main();
 }
