89524fb76b223c6f09218ee356294dba8ce94a87
jsetton <jeremy.setton@gmail.com>
2022-12-31 21:58:07 -0500

    Update copyright 2023
    Signed-off-by: jsetton <jeremy.setton@gmail.com>

--- lambda/utils.js
+++ lambda/utils.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2022 Contributors to the openHAB project
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -11,7 +11,7 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 
-import { deflateSync, inflateSync } from 'zlib';
+import { deflateSync, inflateSync } from 'node:zlib';
 
 /**
  * Returns clamp value
